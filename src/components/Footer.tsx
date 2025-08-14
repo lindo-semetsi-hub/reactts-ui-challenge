3c845273a5eae516d0b877ab4f466efb03eeb9dd
lindo-semetsi-hub <lindosemetsi@gmail.com>
2025-08-14 04:13:53 +0200

    Save Footer changes

---
+++ src/components/Footer.tsx
@@ -0,0 +1,36 @@
+import type React from "react";
+import "./../styles/footer.css";
+
+const Footer: React.FC = () => {
+    return (
+        <footer className="footer">
+            <div className="footer-col">
+                <h4>Premium Restuarant</h4>
+                <p>Fresh, healthy and organic meals served daily.</p>
+            </div>
+            <div className="footer-col">
+                <h4>Our Menu Benefit</h4>
+                <ul>
+                    <li>Fresh</li>
+                    <li>Vitamin</li>
+                    <li>Organic</li>
+                </ul>
+            </div>
+            <div className="footer-col">
+                <h4>Opening Hours</h4>
+                <p>Mon: 17:00 - 21:00</p>
+                <p>Thu: 13:00 - 21:00</p>
+            </div>
+            <div className="footer-col">
+                <h4>Quick Links</h4>
+                <ul>
+                    <li>Home</li>
+                    <li>Menu</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;

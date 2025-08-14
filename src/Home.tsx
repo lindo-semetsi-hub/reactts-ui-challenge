9ee555703b84880e557f2718715a5f75c0d1fa5e
lindo-semetsi-hub <lindosemetsi@gmail.com>
2025-08-14 05:03:05 +0200

    commiting current changes

---
+++ src/Home.tsx
@@ -0,0 +1,35 @@
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import Section from "./components/Section";
+import Card from "./components/Card";
+import Footer from "./components/Footer";
+import type React from "react";
+
+const Home: React.FC = () => {
+    return (
+        <>
+        <Navbar />
+        <HeroSection
+        title="Anida Dedelay"
+        subtitle="Best healthy salad served in our restaurant"
+        buttonLabel="Learn More"
+        imageUrl="/images/bowl.png"
+        />
+        <Section
+        title="Orange Benefit"
+        description="Just reogine eseds and summer in golled quasiunp."
+        buttonLabel="Learn More"
+        leftImage="images/orange.png"
+        rightImage="/images/juice.png"
+        />
+        <div className="card-grid">
+            <Card imageUrl="/images/oranges.jpg" title="ARCU VOLUT FAT VITAE" description="Phestiers 1geart" />
+            <Card imageUrl="/images/orange-slice.jpg" title="$19,99,90.99E" description="Orviste past bronps" />
+            <Card imageUrl="/images/berries.jpg" title="SFIELY TUN DRENARD" description="Ongand suniers" />
+        </div>
+        <Footer />
+        </>
+    );
+};
+
+export default Home;
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,23 +11,21 @@ const Footer: React.FC = () => {
             <div className="footer-col">
                 <h4>Our Menu Benefit</h4>
                 <ul>
-                    <li>Fresh</li>
-                    <li>Vitamin</li>
-                    <li>Organic</li>
+                    <li>FRESH</li>
+                    <li>VITAMIN</li>
+                    <li>ORE ANIC</li>
                 </ul>
             </div>
             <div className="footer-col">
                 <h4>Opening Hours</h4>
-                <p>Mon: 17:00 - 21:00</p>
-                <p>Thu: 13:00 - 21:00</p>
+                <p>Mandday 17.00 cm  x12</p>
+                <p>Tlsueday 13.60 cm X12</p>
+                <p>Wednsseday 19.00 km X11</p>
+                <p>Theneday 13.00 cm X 11</p>
             </div>
             <div className="footer-col">
                 <h4>Quick Links</h4>
-                <ul>
-                    <li>Home</li>
-                    <li>Menu</li>
-                    <li>Contact</li>
-                </ul>
+            
             </div>
         </footer>
     );

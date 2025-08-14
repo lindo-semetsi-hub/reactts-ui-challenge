3e77b4572386badecfd71ca5eaa45fe5551dee38
lindo-semetsi-hub <lindosemetsi@gmail.com>
2025-08-14 03:58:55 +0200

    Save current changes

---
+++ src/components/Section.tsx
@@ -0,0 +1,30 @@
+import type React from "react";
+import "./../styles/section.css";
+
+type SectionProps = {
+    title: string;
+    description: string;
+    buttonLabel: string;
+    leftImage: string;
+    rightImage?: string;
+};
+
+const Section: React.FC<SectionProps> =  ({ title, description, buttonLabel, leftImage, rightImage }) => {
+    return (
+        <section className="info-section">
+        <div className="left">
+        <img src={leftImage} alt={title} />
+        <h2>{title}</h2>
+        <p>{description}</p>
+        <button className="btn-primary">{buttonLabel}</button>
+        </div>
+        {rightImage && (
+            <div className="right">
+                <img src={rightImage} alt="Related" />
+            </div>
+        )}
+        </section>
+    );
+    };
+
+    export default Section;
